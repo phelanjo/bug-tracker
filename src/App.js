96b180f20104da063138cc2a168926eaa2795547
Johnny Phelan <phelanjo@oregonstate.edu>
2020-05-21 14:22:37 -0700

    Add store to hold state.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { useLocalStore } from 'mobx-react';
+
+const store = useLocalStore(() => ({
+  bugs: ["Centipede"]
+}))
 
 function App() {
   return <main>Bugs!</main>
